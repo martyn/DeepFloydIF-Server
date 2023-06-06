1c4da3ef13916b37087867415a1cf21f382c0d2f
Martyn Garcia <martyn.garcia@gmail.com>
2023-06-06 12:30:17 -0600

    return image on endpoint

--- generate.sh
+++ generate.sh
@@ -1,1 +1,37 @@
-curl -X POST -F "stage=2" -F "prompt=An apple." http://localhost:5000/generate_image > /dev/null 
+#!/bin/bash
+
+function print_help {
+    echo "Usage: $0 prompt output_path [num_iterations] [stage]"
+    echo ""
+    echo "Arguments:"
+    echo "  prompt          The prompt to be used for generating the image."
+    echo "  output_path     The path where the output files will be saved."
+    echo "  num_iterations  Optional. The number of times to run the loop. Default is 1."
+    echo "  stage           Optional. The stage parameter to be used in the request. Default is 2."
+    exit 1
+}
+
+if [[ -z "$1" || -z "$2" || "$1" == "-h" || "$1" == "--help" ]]; then
+    print_help
+fi
+
+# The first argument is the prompt.
+prompt=$1
+
+# The second argument is the output path.
+output_path=$2
+
+# The third argument is the number of times to run the loop. If it is not provided, default to 1.
+num_iterations=${3:-1}
+
+# The fourth argument is the stage parameter. If it is not provided, default to 2.
+stage=${4:-2}
+
+for ((i=0; i<$num_iterations; i++))
+do
+  hex=$(openssl rand -hex 3) # Generate a 3-byte (6 characters) random hexadecimal number
+  if ! curl -f -s -S -X POST -F "stage=$stage" -F "prompt=$prompt" http://localhost:5000/generate_image -o "$output_path/apple-$hex.png"; then
+    echo "Error: curl command failed. Please check your server status or the provided arguments."
+    exit 1
+  fi
+done
--- server.py
+++ server.py
@@ -1,38 +1,32 @@
-import os
-from deepfloyd_if.pipelines import dream
-import glob
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 import base64
-from io import BytesIO
+import glob
+import io
+import os
 import torch
-from deepfloyd_if.modules import IFStageI, IFStageII, StableStageIII
-from deepfloyd_if.modules.t5 import T5Embedder
 
 app = Flask(__name__)
 
 device="cuda:0"
-if_I = IFStageI('IF-I-XL-v1.0', device=device)
-if_II = IFStageII('IF-II-L-v1.0', device=device)
-#if_III = StableStageIII('stable-diffusion-x4-upscaler', device=device)
-t5 = T5Embedder(device=device)
-
-
-def save_image(image, stage):
-    output_dir = "output"
-    os.makedirs(output_dir, exist_ok=True)
-    image_files = glob.glob(os.path.join(output_dir, f"*-stage{stage}.png"))
-    image_count = len(image_files) + 1
-    image_path = os.path.join(output_dir, f"{image_count:05d}-stage{stage}.png")
-    image.save(image_path)
-    return image_path
-
+if_I = None
+if_II = None
+t5 = None
 
 @app.route('/generate_image', methods=['POST'])
 def generate_image():
     stage = int(request.form.get('stage'))
     prompt = request.form.get('prompt')
-    print(prompt)
     count=1
+    if if_I is None:
+        from deepfloyd_if.modules import IFStageI, IFStageII, StableStageIII
+        from deepfloyd_if.modules.t5 import T5Embedder
+        from deepfloyd_if.pipelines import dream
+        if_I = IFStageI('IF-I-XL-v1.0', device=device)
+        if_II = IFStageII('IF-II-L-v1.0', device=device)
+        #if_III = StableStageIII('stable-diffusion-x4-upscaler', device=device)
+        t5 = T5Embedder(device=device)
+
+
     result = dream(
         t5=t5, if_I=if_I, if_II=if_II, if_III=None,
         disable_watermark=True,
@@ -53,18 +47,18 @@ def generate_image():
             "sample_timestep_respacing": "75",
         },
     )
-    print(result)
-    save_image(result["I"][0], 1)
-    save_image(result["II"][0], 2)
-    #save_image(result["III"][0], 3)
 
-    # Convert PIL image to base64
-    buffered = BytesIO()
-    #result["III"][0].save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue())
+    byte_arr = io.BytesIO()
+    img.save(byte_arr, format='PNG')
+    byte_arr = byte_arr.getvalue()
+    byte_arr = stream_image(result["III"][0])
 
-    # Return base64 encoded image and image path
-    return jsonify({"image": img_str.decode('utf-8')})
+    print(f"Image created: {image_path}")
+
+    return send_file(
+        io.BytesIO(byte_arr),
+        mimetype='image/png'
+    )
 
 
 if __name__ == '__main__':
